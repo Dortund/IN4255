9380d24c38922b1cf8fd0ec8ae662862931672da
Dortund <noneurbusiness@care.com>
2017-06-09 16:24:36 +0200

    continuted work

--- practical3/src/workshop/ShapeInterpolation.java
+++ practical3/src/workshop/ShapeInterpolation.java
@@ -1,23 +1,33 @@
 package workshop;
 
+import Jama.EigenvalueDecomposition;
+import Jama.Matrix;
+import Jama.SingularValueDecomposition;
 import jv.geom.PgElementSet;
+import jv.vecmath.PdMatrix;
 import jv.vecmath.PdVector;
+import jv.vecmath.PiVector;
 import jvx.project.PjWorkshop;
 
 public class ShapeInterpolation extends PjWorkshop {
 	
 	/** First surface to be registered. */	
-	PgElementSet	m_surfP;
+	PgElementSet	meshOrigin;
     /** Keeps the a copy of the vertices of P when loaded
      * This can be used to reset P.
      */
-    PdVector[] m_surfP_original;
+    PdVector[] meshOriginBackup;
 	/** Second surface to be registered. */
-	PgElementSet	m_surfQ;
+	PgElementSet	meshTarget;
     /** Keeps the a copy of the vertices of Q when loaded
      * This can be used to reset Q.
      */
-    PdVector[] m_surfQ_original;
+    PdVector[] meshTargetBackup;
+    
+    double[] angles;
+    PdVector[] rotationAxes;
+    PdMatrix[] scalings;
+    PdVector[] translations;
 
 	/** Constructor */
 	public ShapeInterpolation() {
@@ -32,12 +42,99 @@ public class ShapeInterpolation extends PjWorkshop {
 		super.init();
 	}
 	
+	public void interpolate(double time) {
+		int numElements = meshOrigin.getNumElements();
+		
+		angles = new double[numElements];
+		rotationAxes = new PdVector[numElements];
+		scalings = new PdMatrix[numElements];
+		translations = new PdVector[numElements];
+		
+		PdMatrix[] transforms = getTransforms();
+		
+		for (int i = 0; i < numElements; i++) {
+			SingularValueDecomposition svd = new SingularValueDecomposition(new Matrix(transforms[i].getEntries()));
+			
+	        Matrix V = svd.getV();
+	        Matrix Vt = svd.getV().transpose();
+	        Matrix U = svd.getU();
+	        Matrix UVt = U.times(Vt);
+	        
+	        double det = UVt.det();
+	        
+	        Matrix Z = Matrix.identity(3, 3);
+	        Z.set(2, 2, det);
+	        
+	        Matrix R = U.times(Z).times(Vt);
+	        Matrix S = V.times(Z).times(svd.getS()).times(Vt);
+	        
+	        scalings[i] = new PdMatrix(S.getArray());
+	        
+	        angles[i] = Math.acos((R.trace()-1)/2.0);
+	        
+	        Matrix temp = R.times(R.transpose()).times(0.5);
+	        EigenvalueDecomposition eig = temp.eig();
+	        for (int v = 0; v < eig.getRealEigenvalues().length; v++){
+	        	if (eig.getRealEigenvalues()[v] == 1 && eig.getImagEigenvalues()[v] == 0) {
+	        		double[] entries = eig.getV().getColumnPackedCopy();
+	        		PdVector axis = new PdVector(3);
+	        		axis.setEntry(0, entries[v*3 + 0]);
+	        		axis.setEntry(1, entries[v*3 + 1]);
+	        		axis.setEntry(2, entries[v*3 + 2]);
+	        		rotationAxes[i] = axis;
+	        	}
+	        }
+	        
+	        PdVector o = meshOrigin.getVertex(meshOrigin.getElement(i).getEntry(0));
+	        PdVector t = meshTarget.getVertex(meshTarget.getElement(i).getEntry(0));
+	        
+	        translations[i] = PdVector.subNew(t, o);
+		}
+	}
+	
+	private PdMatrix[] getTransforms() {
+		meshOrigin.makeElementNormals();
+		meshOrigin.makeElementNormals();
+		
+		PdMatrix[] transforms = new PdMatrix[meshOrigin.getNumElements()];
+		
+		for (int i = 0; i < meshOrigin.getNumElements(); i++) {
+			PdMatrix V = getTransform(meshOrigin, i);
+			PdMatrix W = getTransform(meshTarget, i);
+			
+			V.invert();
+			W.rightMult(V);
+			
+			transforms[i] = W;
+		}
+		
+		return transforms;
+	}
+	
+	private PdMatrix getTransform(PgElementSet mesh, int index) {
+		PiVector faceOrigin = mesh.getElement(index);
+		PdVector p1 = mesh.getVertex(faceOrigin.getEntry(0));
+		PdVector p2 = mesh.getVertex(faceOrigin.getEntry(1));
+		PdVector p3 = mesh.getVertex(faceOrigin.getEntry(2));
+		PdVector nv = mesh.getElementNormal(index);
+		
+		PdVector v1 = PdVector.subNew(p3, p1);
+		PdVector v2 = PdVector.subNew(p2, p1);
+		
+		PdMatrix V = new PdMatrix(3, 3);
+		V.setColumn(0, v1);
+		V.setColumn(1, v2);
+		V.setColumn(2, nv);
+		
+		return V;
+	}
+	
 	/** Set two Geometries. */
 	public void setGeometries(PgElementSet surfP, PgElementSet surfQ) {
-		m_surfP = surfP;
-        m_surfP_original = PdVector.copyNew(surfP.getVertices());
-		m_surfQ = surfQ;
-        m_surfQ_original = PdVector.copyNew(surfQ.getVertices());
+		meshOrigin = surfP;
+        meshOriginBackup = PdVector.copyNew(surfP.getVertices());
+		meshTarget = surfQ;
+        meshTargetBackup = PdVector.copyNew(surfQ.getVertices());
         System.out.println(String.format("Vertices P:%d vertices Q: %d", surfP.getVertices().length, surfQ.getVertices().length));
 	}
 	
@@ -46,10 +143,10 @@ public class ShapeInterpolation extends PjWorkshop {
      * No need to call update
      */
 	public void reset() {
-	    m_surfP.setVertices(m_surfP_original.clone());
-        m_surfP.update(m_surfP);
+	    meshOrigin.setVertices(meshOriginBackup.clone());
+        meshOrigin.update(meshOrigin);
 
-        m_surfQ.setVertices(m_surfQ_original.clone());
-        m_surfQ.update(m_surfQ);
+        meshTarget.setVertices(meshTargetBackup.clone());
+        meshTarget.update(meshTarget);
     }
 }
---
+++ practical3/src/workshop/util.java
@@ -0,0 +1,6 @@
+package workshop;
+
+public class util {
+	
+	
+}
